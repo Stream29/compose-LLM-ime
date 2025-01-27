50dd799f921b92a1e678c292ea66fab57507e287
Stream <1542763342@qq.com>
2025-01-27 21:59:01 +0800

    chore: suppress warning

--- app/src/main/kotlin/com/example/composeime/view/Theme.kt
+++ app/src/main/kotlin/com/example/composeime/view/Theme.kt
@@ -1,11 +1,13 @@
 package com.example.composeime.view
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 
+@SuppressLint("NewApi")
 @Composable
 fun ComposeIMETheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colorScheme = if (darkTheme) {
@@ -19,5 +21,4 @@ fun ComposeIMETheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
         shapes = Shapes(),
         content = content
     )
-
 }
