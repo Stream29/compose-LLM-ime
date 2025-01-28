1a7a71e0a5b50d64ba489f90f32a4939dba00af0
Stream <1542763342@qq.com>
2025-01-29 02:56:47 +0800

    refactor: remove redundant layout

--- app/src/main/kotlin/com/example/composeime/view/screen/KeyboardScreen.kt
+++ app/src/main/kotlin/com/example/composeime/view/screen/KeyboardScreen.kt
@@ -3,15 +3,12 @@ package com.example.composeime.view.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeime.view.ComposeIMETheme
-import com.example.composeime.view.component.FixedHeightBox
 import com.example.composeime.view.component.KeyboardKey
 
 @Preview
@@ -30,18 +27,18 @@ fun KeyboardScreen() = ComposeIMETheme {
     ) {
         val keyWidth = LocalConfiguration.current.screenWidthDp.dp / 10
         keysMatrix.forEach { row ->
-            FixedHeightBox(
-                modifier = Modifier.fillMaxWidth(), height = 56.dp
+            Row(
+                modifier = Modifier
+                    .requiredHeight(56.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
             ) {
-                Row(
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    row.forEach { key ->
-                        KeyboardKey(keyboardKey = key, modifier = Modifier.widthIn(max = keyWidth))
-                    }
+                row.forEach { key ->
+                    KeyboardKey(keyboardKey = key, modifier = Modifier.widthIn(max = keyWidth))
                 }
             }
         }
+
     }
 }
 
--- app/src/main/kotlin/com/example/composeime/view/screen/SettingsScreen.kt
+++ app/src/main/kotlin/com/example/composeime/view/screen/SettingsScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeime.view.ComposeIMETheme
+import splitties.systemservices.inputManager
 import splitties.systemservices.inputMethodManager
 
 @Preview
