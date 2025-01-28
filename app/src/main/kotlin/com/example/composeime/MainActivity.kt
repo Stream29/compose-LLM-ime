bfee0baf84a1b0307e0e38b372df471093af052f
Stream <1542763342@qq.com>
2025-01-29 03:35:34 +0800

    feat: add top bar

--- app/src/main/kotlin/com/example/composeime/MainActivity.kt
+++ app/src/main/kotlin/com/example/composeime/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.composeime
 
-import SettingsScreen
+import com.example.composeime.view.screen.SettingsScreen
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/kotlin/com/example/composeime/view/screen/SettingsScreen.kt
+++ app/src/main/kotlin/com/example/composeime/view/screen/SettingsScreen.kt
@@ -1,66 +1,71 @@
+package com.example.composeime.view.screen
+
 import android.content.Intent
-import android.provider.Settings
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeime.view.ComposeIMETheme
-import splitties.systemservices.inputManager
 import splitties.systemservices.inputMethodManager
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun SettingsScreen() = ComposeIMETheme {
-    Surface(color = MaterialTheme.colorScheme.background) {
-        Column {
-            Options()
-            Spacer(modifier = Modifier.weight(1f))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Compose IME") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+            )
         }
-    }
-}
-
-@Composable
-fun Options() {
-    Column(
-        Modifier.padding(16.dp).fillMaxWidth()
-    ) {
-        val ctx = LocalContext.current
-        Text(text = "Compose Keyboard")
-        val (text, setValue) = remember { mutableStateOf(TextFieldValue("Try here")) }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = { ctx.startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)) }
+    ) { innerPadding ->
+        Column(
+            Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(MaterialTheme.colorScheme.surface),
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Enable IME")
+            val ctx = LocalContext.current
+            val (text, setValue) = remember<MutableState<TextFieldValue>> {
+                mutableStateOf<TextFieldValue>(
+                    TextFieldValue("Try here")
+                )
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { ctx.startActivity(Intent(ACTION_INPUT_METHOD_SETTINGS)) }
+            ) {
+                Text(text = "Enable IME")
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { inputMethodManager.showInputMethodPicker() }
+            ) {
+                Text(text = "Select IME")
+            }
+            TextField(
+                value = text,
+                onValueChange = setValue,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = { inputMethodManager.showInputMethodPicker() }
-        ) {
-            Text(text = "Select IME")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            value = text,
-            onValueChange = setValue,
-            modifier = Modifier.fillMaxWidth()
-        )
     }
 }
