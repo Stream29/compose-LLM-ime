37ee9c3816c18e37c8d2e73b3e68b94251a2b203
Stream <1542763342@qq.com>
2025-01-29 08:55:51 +0800

    refactor: correct viewModel usage

--- app/src/main/kotlin/com/example/composeime/MainActivity.kt
+++ app/src/main/kotlin/com/example/composeime/MainActivity.kt
@@ -4,14 +4,15 @@ import com.example.composeime.view.screen.SettingsScreen
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.remember
+import com.example.composeime.viewmodel.SettingsViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SettingsScreen()
+            val viewModel = remember { SettingsViewModel() }
+            SettingsScreen(viewModel)
         }
     }
 }
-
-
